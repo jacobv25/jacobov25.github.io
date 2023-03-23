bbd0d68668c2408f7ae7ed2d35655642ff6a790e
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2023-03-23 13:18:42 -0700

    Update rolltable.js
    added a button

--- js/rolltable.js
+++ js/rolltable.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  $('.rollable-table tbody tr').click(function() {
-    var roll = $(this).find('td:first-child').text();
-    var result = $(this).find('td:last-child').text();
+  $('#roll-button').click(function() {
+    var roll = $('.rollable-table tbody tr.active').find('td:first-child').text();
+    var result = $('.rollable-table tbody tr.active').find('td:last-child').text();
     alert('You rolled ' + roll + '. Result: ' + result);
+  });
+  
+  $('.rollable-table tbody tr').click(function() {
+    $(this).addClass('active').siblings().removeClass('active');
   });
 });
